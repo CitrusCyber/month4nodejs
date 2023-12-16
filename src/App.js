07f1649f028d91f01a6be68371d2f6d6c26688a6
Artur <arry05@bk.ru>
2023-12-16 09:34:35 +0400

    DZ02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>hello WORLD</h1>
-    </div>
-  );
+import { useState } from "react"
+
+function ColorToggleForm() {
+    const [color1, setColor1] = useState('red')
+    const [color2, setColor2] = useState('blue')
+
+    const toggleColors = () => {
+        setColor1(color2)
+        setColor2(color1)
+    }
+
+    return (
+        <div>
+            <div
+                style={{
+                    width: '100px',
+                    height: '100px',
+                    margin: '10px',
+                    backgroundColor: color1,
+                    cursor: 'pointer'
+                }}
+                onClick={toggleColors}
+            ></div>
+            <div
+                style={{
+                    width: '100px',
+                    height: '100px',
+                    margin: '10px',
+                    backgroundColor: color2,
+                    cursor: 'pointer'
+                }}
+                onClick={toggleColors}
+            ></div>
+        </div>
+    )
 }
 
-export default App;
-
-
+export default ColorToggleForm
