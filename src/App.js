7ab09edf14fa9a08868368bd09b71d52b3491232
Artur <arry05@bk.ru>
2023-12-22 23:09:36 +0400

    DZ04

--- src/App.js
+++ src/App.js
@@ -1,35 +1,75 @@
 import React, { useState } from 'react';
 
-const TodoList = () => {
-    const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTask] = useState('');
+const AuthForm = () => {
+    const [formData, setFormData] = useState({
+        fullName: '',
+        age: '',
+        email: '',
+        phoneNumber: ''
+    });
 
-    const handleAddTask = () => {
-        if (newTask.trim() !== '') {
-            setTasks([...tasks, newTask]);
-            setNewTask('');
-        }
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
     };
 
     return (
         <div>
-            <h1>Todo List</h1>
-            <ul>
-                {tasks.map((task, index) => (
-                    <li key={index}>{task}</li>
-                ))}
-            </ul>
+            <h2>Регистрация/Авторизация</h2>
+            <form>
+                <label>
+                    ФИО:
+                    <input
+                        type="text"
+                        name="fullName"
+                        value={formData.fullName}
+                        onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Возраст:
+                    <input
+                        type="number"
+                        name="age"
+                        value={formData.age}
+                        onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Электронная почта:
+                    <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Номер телефона:
+                    <input
+                        type="tel"
+                        name="phoneNumber"
+                        value={formData.phoneNumber}
+                        onChange={handleChange}
+                    />
+                </label>
+            </form>
+
             <div>
-                <input
-                    type="text"
-                    value={newTask}
-                    onChange={(event) => setNewTask(event.target.value)}
-                    placeholder="Вводи что-то:"
-                />
-                <button onClick={handleAddTask}>Добавить !</button>
+                <h3>Данные пользователя в реальном времени:</h3>
+                <p>ФИО: {formData.fullName}</p>
+                <p>Возраст: {formData.age}</p>
+                <p>Электронная почта: {formData.email}</p>
+                <p>Номер телефона: {formData.phoneNumber}</p>
             </div>
         </div>
     );
 };
 
-export default TodoList;
+export default AuthForm;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TodoList from './App';
+import AuthForm from "./App";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoList />
+    <AuthForm />
   </React.StrictMode>
 );
 
